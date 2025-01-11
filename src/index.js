b260e8fe2b96c84ab4cde3b8dcf68412ff92468b
m-aular <marionaular@gmail.com>
2025-01-11 23:42:52 +0000

    console logged temperature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 function refreshWeather(response) {
-  console.log(response.data);
+  console.log(response.data.temperature.current);
 }
 
 function searchCity(city) {
   let apiKey = "c77488431cbt108ado707a56be20bd2f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
