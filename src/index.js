85974398fbc7ac61b6196d674b4757738527b105
m-aular <marionaular@gmail.com>
2025-01-11 23:37:46 +0000

    added function to display weather data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
 function searchCity(city) {
   let apiKey = "c77488431cbt108ado707a56be20bd2f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
