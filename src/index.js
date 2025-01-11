1a2df8a140a6e7b505233f36ca686d13acff716e
m-aular <marionaular@gmail.com>
2025-01-11 23:50:20 +0000

    added temperature element

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function refreshWeather(response) {
-  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
 }
 
 function searchCity(city) {
