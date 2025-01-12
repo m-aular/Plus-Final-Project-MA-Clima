16c631a8d9e15dbef94e23ae817d6c8a4ae6de10
m-aular <marionaular@gmail.com>
2025-01-12 00:07:44 +0000

    amended typo

--- src/index.js
+++ src/index.js
@@ -23,4 +23,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searachCity("Leeds");
+searchCity("Leeds");
